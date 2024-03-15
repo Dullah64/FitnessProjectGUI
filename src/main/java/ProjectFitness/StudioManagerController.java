2a060dfe9464930e9afc45608ffa5a94a8a057f5
Nikhilesh Madduri <nnm50@scarletmail.rutgers.edu>
2024-03-15 00:32:18 -0400

    Added 2nd page controllers, but facing issues

--- src/main/java/ProjectFitness/StudioManagerController.java
+++ src/main/java/ProjectFitness/StudioManagerController.java
@@ -7,12 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -26,9 +24,9 @@ public class StudioManagerController {
     @FXML
     private TextField lastnameinput;
     @FXML
-    private Label memeberLabel;
+    private Label memberLabel;
     @FXML
-    private Button cancelmemebership;
+    private Button cancelmembership;
     @FXML
     private DatePicker DatePicker;
     @FXML
@@ -36,15 +34,14 @@ public class StudioManagerController {
     @FXML
     private Label guestPass;
     @FXML
-            private Button addmember;
+    private Button addmember;
     @FXML
-    private TextArea commandline2; // for the output area in memebership tab..
-    @FXML
-    private Button loadMembers; // to load the member list in the membershiptab
+    private TextArea commandline;
+
     String fname;
     String lname;
     String dob;
-    int numberGuest;
+    int numberofGuestPasses;
 
     @FXML   // Getting MemberShip Type Radio Buttons
     private RadioButton rbbasic, rbfamily, rbpremium;
@@ -85,15 +82,15 @@ public class StudioManagerController {
             Profile profile = new Profile(fname, lname, dob1);
             Member member = new Member(profile);
             if (members.remove(member)) {
-                commandline2.appendText(fname + " " + lname + " removed." + "\n");
+                System.out.println(fname + " " + lname + " removed.");
             } else {
-                commandline2.appendText(fname + " " + lname + " not in database." +"\n");
+                System.out.println(fname + " " + lname + " not in database.");
             }
 
         }
 
 
-        public void addmember(ActionEvent event) { // Add conditions including guest part..
+        public void addmember(ActionEvent event){ // Add conditions including guest part..
             fname = firstnameinput.getText();
             lname = lastnameinput.getText();
             dob = DatePicker.getValue().toString();
@@ -102,57 +99,33 @@ public class StudioManagerController {
             String numberGuest = guest.getText(); // Number of guest Passes..
 
 
+
+
+
             Date dob1 = parseDateOfBirth(dob);
             Location loc = Location.valueOf(getHomeLocation().toUpperCase());
-            Date exp = new Date(4, 14, 2024);
+            Date exp = new Date(4,14,2024);
             Profile profile = new Profile(fname, lname, dob1);
 
             if (!isOldEnough(dob1)) {
-                commandline2.appendText("DOB " + dob1 + ": must be 18 or older to join!" + "\n");
+                System.out.println("DOB " + dob1 + ": must be 18 or older to join!");
             }
 
-            if (getMemberShipType().equals("Basic")) {
-                Basic basic = new Basic(profile, exp, loc);
+            if (getMemberShipType().equals("Basic")){
+                Basic basic = new Basic(profile,exp,loc);
                 members.add(basic);
-                commandline2.appendText("Member Added" + "\n");
+                System.out.println("Member Added");
             }
-            if (getMemberShipType().equals("Family")) {
-                Family family = new Family(profile, exp, loc);
+            if (getMemberShipType().equals("Family")){
+                Family family = new Family(profile,exp,loc);
                 members.add(family);
             }
-            if (getMemberShipType().equals("Premium")) {
-                Premium premium = new Premium(profile, exp, loc);
+            if (getMemberShipType().equals("Premium")){
+                Premium premium = new Premium(profile,exp,loc);
                 members.add(premium);
             }
-        }
-        @FXML
-            public void loadMembers(ActionEvent event) throws IOException {  // MemberList Button
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Select Members File");
 
-                // Show open file dialog
-                Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-                File selectedFile = fileChooser.showOpenDialog(stage);
-                if (selectedFile != null) {
-
-                    members.load(selectedFile);
-                    Member[] membersarray = members.getmembers();
-                    //  Output Members
-                    for (int i = 0; i < members.getsize(); i++){ // change this magic number
-                        commandline2.appendText(membersarray[i].toString() + "\n");}
-                } else {
-
-                    System.out.println("No file selected.");
-                }
-
-
-
-
-
-
-            }
-
-            // Do it here
+    }
         private Date parseDateOfBirth (String dob){
             String[] dobparts = dob.split("-");
             int dobmonth = Integer.parseInt(dobparts[0]);
@@ -167,63 +140,8 @@ public class StudioManagerController {
         return today.compareTo(eighteenYearsAgo) >= 0;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private Schedule schedule1;
-    private MemberList member;
     @FXML
     private TextArea resultText;
-    @FXML
-    private TextField firstName;
-    @FXML
-    private TextField lastName;
-    @FXML
-    private DatePicker dob1;
-    @FXML
-    private TextField guest_pass;
-    @FXML
-    private RadioButton BasicButton;
-    @FXML
-    private RadioButton FamilyButton;
-    @FXML
-    private RadioButton PremiumButton;
-    @FXML
-    private RadioButton BridgewaterButton;
-    @FXML
-    private RadioButton SomervilleButton;
-    @FXML
-    private RadioButton PiscatawayButton;
-    @FXML
-    private RadioButton FranklinButton;
-    @FXML
-    private RadioButton EdisonButton;
     @FXML
     private RadioButton CardioButton;
     @FXML
@@ -240,6 +158,168 @@ public class StudioManagerController {
     private RadioButton DeniseButton;
     @FXML
     private RadioButton EmmaButton;
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField lastname;
+    @FXML
+    private DatePicker datepicker;
+
+    @FXML
+    public RadioButton Bridgewater, Somerville, Piscataway, Franklin, Edison;
+    public String gethomelocation() {
+        if (Bridgewater.isSelected()) {
+            return "BridgeWater";
+        }
+        if (Somerville.isSelected()) {
+            return "Somerville";
+        }
+        if (Piscataway.isSelected()) {
+            return "Piscataway";
+        }
+        if (Franklin.isSelected()) {
+            return "Franklin";
+        }
+        return "Edison";
+    }
+    public String getclass(){
+        if(CardioButton.isSelected()){
+            return "Cardio";
+        }
+        if(PilatesButton.isSelected()){
+            return "Pilates";
+        }
+        return "Spinning";
+    }
+    public String getInstructor(){
+        if(EmmaButton.isSelected()){
+            return "Emma";
+        }
+        if(JenniferButton.isSelected()){
+            return "Jennifer";
+        }
+        if(KimButton.isSelected()){
+            return "Kim";
+        }
+        if(DeniseButton.isSelected()){
+            return "Denise";
+        }
+        return "Davis";
+    }
+
+    public void addmembertoclass(){
+        fname = firstname.getText();
+        lname = lastname.getText();
+        dob = datepicker.getValue().toString();
+
+        // Set Conditions and Give Boundries to it..
+        String numberGuest = guest.getText(); // Number of guest Passes..
+        FitnessClass[] schArray = schedule.getclasses();
+        Date dob1 = parseDateOfBirth(dob);
+        Location loc = Location.valueOf(gethomelocation().toUpperCase());
+        Date exp = new Date(4,14,2024);
+        Profile profile = new Profile(fname, lname, dob1);
+        Member member = new Member(profile);
+        for (int i = 0; i <schArray.length; i++) {
+            if (schArray[i].getoffer().name().equals(getclass().toUpperCase()) &&
+                    schArray[i].getIns().name().equals(getInstructor().toUpperCase())) {
+                if (schArray[i].getmembers().contains(member)) {
+                    resultText.setText(fname + " " + lname + " is already registered for " +
+                            getclass().toUpperCase() + " with " + getInstructor().toUpperCase());
+                    return;
+                } else {
+                    schArray[i].getmembers().add(member);
+                    resultText.setText(fname + " " + lname + " is registered for " + getclass().toUpperCase() +
+                            " with " + getInstructor().toUpperCase() + " at " + gethomelocation().toUpperCase());
+                    return;
+                }
+            }
+        }
+        resultText.setText("Fitness class not on class schedule");
+    }
+    public void removememberfromclass(){
+        fname = firstname.getText();
+        lname = lastname.getText();
+        dob = datepicker.getValue().toString();
+
+        // Set Conditions and Give Boundries to it..
+        String numberGuest = guest.getText(); // Number of guest Passes..
+        FitnessClass[] schArray = schedule.getclasses();
+        Date dob1 = parseDateOfBirth(dob);
+        Location loc = Location.valueOf(gethomelocation().toUpperCase());
+        Date exp = new Date(4,14,2024);
+        Profile profile = new Profile(fname, lname, dob1);
+        Member member = new Member(profile);
+
+        for (int i = 0; i < schArray.length; i++) {
+            if (schArray[i].getmembers().contains(member)) {
+                schArray[i].getmembers().remove(member);
+                resultText.setText(fname + " " + lname + " is unregistered from the class");
+                return;
+            }
+        }
+        resultText.setText(fname + " " + lname + " is not registered for any classes");
+    }
+    public void addguest(){
+        fname = firstname.getText();
+        lname = lastname.getText();
+        dob = datepicker.getValue().toString();
+
+        // Set Conditions and Give Boundries to it..
+        String numberGuest = guest.getText(); // Number of guest Passes..
+        FitnessClass[] schArray = schedule.getclasses();
+        Date dob1 = parseDateOfBirth(dob);
+        Location loc = Location.valueOf(gethomelocation().toUpperCase());
+        Date exp = new Date(4,14,2024);
+        Profile profile = new Profile(fname, lname, dob1);
+        Member member = new Member(profile);
+        for (int i = 0; i <schArray.length; i++) {
+            if (schArray[i].getoffer().name().equals(getclass().toUpperCase()) &&
+                    schArray[i].getIns().name().equals(getInstructor().toUpperCase())) {
+                if (schArray[i].getguests().contains(member)) {
+                    resultText.setText(fname + " " + lname + " is already registered as a guest for " +
+                            getclass().toUpperCase() + " with " + getInstructor().toUpperCase());
+                    return;
+                } else {
+                    schArray[i].getguests().add(member);
+                    System.out.println(fname + " " + lname + " is registered as a guest for " + getclass().toUpperCase() +
+                            " with " + getInstructor().toUpperCase() + " at " + gethomelocation().toUpperCase());
+                    return;
+                }
+            }
+        }
+        System.out.println("Fitness class not on class schedule");
+    }
+    public void removeguest(){
+        fname = firstname.getText();
+        lname = lastname.getText();
+        dob = datepicker.getValue().toString();
+
+        // Set Conditions and Give Boundries to it..
+        String numberGuest = guest.getText(); // Number of guest Passes..
+        FitnessClass[] schArray = schedule.getclasses();
+        Date dob1 = parseDateOfBirth(dob);
+        Location loc = Location.valueOf(gethomelocation().toUpperCase());
+        Date exp = new Date(4,14,2024);
+        Profile profile = new Profile(fname, lname, dob1);
+        Member member = new Member(profile);
+        for (int i = 0; i <schArray.length; i++) {
+            if (schArray[i].getoffer().name().equals(getclass().toUpperCase()) &&
+                    schArray[i].getIns().name().equals(getInstructor().toUpperCase())) {
+                if (schArray[i].getguests().contains(member)) {
+                    schArray[i].getguests().remove(member);
+                    resultText.setText(fname + " " + lname + " is unregistered as a guest for " +
+                            getclass().toUpperCase() + " with " + getInstructor().toUpperCase());
+                    return;
+                } else {
+                    resultText.setText(fname + " " + lname + " is not registered as a guest for " +
+                            getclass().toUpperCase() + " with " + getInstructor().toUpperCase());
+                    return;
+                }
+            }
+        }
+        System.out.println("Fitness class not on class schedule");
+    }
     @FXML
     MenuItem showSchedule;
     @FXML
